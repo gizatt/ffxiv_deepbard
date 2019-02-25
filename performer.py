3789ea34e6b7d437354b37bd32af0602f6ccfb90
Greg Izatt <gizatt@mit.edu>
2019-02-24 22:46:24 -0500

    Performer tweaks from my last session

--- performer.py
+++ performer.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 import IPython
+import matplotlib.pyplot as plt
 import numpy as np
 import pretty_midi
 import pynput
@@ -17,14 +18,14 @@ class MonophonicPerformer():
     real-time playing of the queued slices against the system clock.
     Manages priotiziation of voices to play. '''
     # Base note of C4
-    def __init__(self, keys_string=default_keys_string, base_note=12*4,
-                 beat_divisions=4, buffer_size=1024, default_tempo=120):
+    def __init__(self, keys_string=default_keys_string,
+                 beat_divisions=4, buffer_size=1000, default_tempo=90):
         self.keys_string = keys_string
         self.beat_divisions = beat_divisions
         self.num_keys = len(self.keys_string)
         self.playing = False
         self.buffer_size = buffer_size
-        self.note_buffer = np.zeros(self.num_keys, self.buffer_size)
+        self.note_buffer = np.zeros((self.num_keys, self.buffer_size))
         self.tempo_buffer = np.ones(self.buffer_size)*default_tempo
         self.lock = Lock()
         self.playing_thread = None
@@ -35,34 +36,35 @@ class MonophonicPerformer():
         if piano_slice.shape != (self.num_keys,):
             raise ValueError("Slice was wrong shape: got %s" % piano_slice.shape)
         with self.lock:
-            if slice_index <= self.note_buffer_head:
+            if slice_index < self.note_buffer_head:
                 print("This slice is in the past, skipping.")
                 return
-            write_ind = (self.note_buffer_head + slice_index) % self.num_keys
-            self.note_buffer[:, write_ind] = piano_roll[:]
+            elif (slice_index >= self.note_buffer_head + self.buffer_size):
+                print("This slice is too far in the future, skipping.")
+            write_ind = slice_index % self.buffer_size
+            self.note_buffer[:, write_ind] = piano_slice[:]
             if tempo is not None:
                 self.tempo_buffer[write_ind] = tempo
 
     def decideAndExecuteAction(self, current_slice, last_slice, last_note):
         # Dumb baseline:
-        # If the last note was nothing or is not continued,
-        # take the highest note in the current slice.
-        if last_note is None or current_slice[last_note] == 0:
+        # Take the highest note in the current slice.
+        # print("Slice: ", current_slice)
+        if not np.any(current_slice):
+            new_note = None
+        else:
             candidates = np.nonzero(current_slice)
-            if len(candidates) > 0:
-                new_note = candidates[-1]
-            else:
-                new_note = None
-        else: 
-            # Otherwise continue the current note
-            new_note = last_note
+            new_note = candidates[-1][0]    
         
+        # print("Last note: %s, curr note: %s" % (str(last_note), str(new_note)))
         # Release events
-        if last_note is not None and new_note is not last_note:
+        if last_note != None and new_note != last_note:
+            #print("Releasing")
             self.keyboard.release(self.keys_string[last_note])
         
         # Press events
-        if new_note is not None and new_note is not last_note:
+        if new_note != None and new_note != last_note:
+            #print("Pressing")
             self.keyboard.press(self.keys_string[new_note])
 
         return new_note
@@ -79,18 +81,21 @@ class MonophonicPerformer():
         while self.playing:
             with self.lock:
                 last_slice = current_slice
-                current_slice = self.note_buffer[:, self.note_buffer_head]
-                current_tempo = self.tempo_buffer[self.note_buffer_head]
-                self.note_buffer_head = (self.note_buffer_head + 1) % self.num_keys
+                read_index = self.note_buffer_head % self.buffer_size
+                current_slice = self.note_buffer[:, read_index].copy()
+                self.note_buffer[:, read_index]*=0
+                current_tempo = self.tempo_buffer[read_index]
+                self.note_buffer_head += 1
 
-            # Given current and last note to play, decide current action.
-            last_note = self.decideAndExecuteAction(current_slice, last_slice, last_note)
             # Tempo is in BPM
             next_note_time = last_note_time + 60. / (current_tempo * self.beat_divisions)
-            last_note_time = time.time()
             t = next_note_time - time.time()
-            if t > 0:
-                time.sleep(t)
+            while t > 0:
+                t = next_note_time - time.time()
+
+            # Given current and last note to play, decide current action.
+            last_note_time = time.time()
+            last_note = self.decideAndExecuteAction(current_slice, last_slice, last_note)
 
     def wait_for_stop_playing(self, final_index):
         while True:
@@ -106,52 +111,26 @@ class MonophonicPerformer():
 
 if __name__ == "__main__":
     try:
-        MIDI_PATH = "data/Everytime_We_Touch.mid"
+        MIDI_PATH = "data/FF1Matoya.mid"
         piano_roll, tempo_roll = midi_filename_to_piano_roll(MIDI_PATH)
         midi_data = pretty_midi.PrettyMIDI(MIDI_PATH)
-        
-        notes = []
-        for instrument_num in [0, 1]:
-            print("Instrument %d has %d notes" % (instrument_num,len(midi_data.instruments[instrument_num].notes)))
-            notes += midi_data.instruments[instrument_num].notes
-        print("%d notes total" % len(notes))
 
-        # Clear out notes that are lower simultaneous with other notes
-        good_notes_mask = []
-        for note_i in range(len(notes)):
-            this_start = notes[note_i].start
-            this_end = notes[note_i].end
-            this_pitch = notes[note_i].pitch
-            reject = False
-            for other_note_i in range(len(notes)):
-                if note_i == other_note_i:
-                    continue
-                if (notes[other_note_i].start >= this_start - 0.01 and
-                    notes[other_note_i].start <= this_start + 0.2 and
-                    notes[other_note_i].pitch > this_pitch):
-                    reject = True
-                    print("Note %s, rejecting bc note %s" % (str(notes[other_note_i]), str(notes[note_i])))
-                    continue
-            if not reject:
-                good_notes_mask.append(note_i)
-
-        notes = [notes[x] for x in good_notes_mask]
-        notes = sorted(notes, key=lambda x: x.start)
-        intervals = np.array([[note.start, note.end] for note in notes])
-        pitches = np.array([note.pitch for note in notes])
+        print("Piano roll shape: ", piano_roll.shape)
+        print("Tempo roll shape: ", tempo_roll.shape)
 
         c4 = 12*4
-        performer = MonophonicPerformer(base_note=c4)
-        for i in range(len(notes)):
-            performer.enqueue(
-                NoteQueueEntry(
-                    note=pitches[i]-12*4,
-                    start=intervals[i][0],
-                    stop=intervals[i][1]))
+        c7 = 12*7
 
+        #plt.figure().set_size_inches(12, 12)
+        #plt.imshow(piano_roll[c4:(c7+1), 0:200])
+        #plt.show()
+        
+        performer = MonophonicPerformer()
+        for i in range(piano_roll.shape[1]):
+            performer.set_piano_roll_slice(i, piano_roll[c4:(c7+1), i], tempo_roll[i])
         print("STARTING...")
         performer.start_playing()
-        performer.wait_for_stop_playing()
+        performer.wait_for_stop_playing(i+1)
 
     except KeyboardInterrupt as e: 
         print("Keyboard interrupt, shutting down.")
